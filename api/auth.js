8021c94b6aa696c94375d1b75a7772bbb930d788
Brandon Hamilton <bham2015@gmail.com>
2024-10-16 12:49:39 -0500

    Completed Guided Practice.

--- api/auth.js
+++ api/auth.js
@@ -2,8 +2,19 @@ const express = require("express");
 const router = express.Router();
 
 // TODO: Import jwt and JWT_SECRET
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = process.env.JWT_SECRET;
 
 // TODO: createToken
+async function createToken(id) {
+  jwt.sign(
+    {
+      id,
+    },
+    JWT_SECRET,
+    { expiresIn: "1d" }
+  );
+}
 
 const prisma = require("../prisma");
 
@@ -15,12 +26,41 @@ router.use(async (req, res, next) => {
   const token = authHeader?.slice(7); // "Bearer <token>"
   if (!token) return next();
 
-  // TODO: Find customer with ID decrypted from the token and attach to the request
+  try {
+    const { id } = jwt.verify(token, JWT_SECRET);
+    const customer = await prisma.customer.findUniqueOrThrow({
+      where: { id },
+    });
+    req.customer = customer;
+    next();
+  } catch (error) {
+    next(error);
+  }
 });
 
 // TODO: POST /register
+router.post("/register", async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const customer = await prisma.customer.register(email, password);
+    const token = createToken(customer.id);
+    res.status(201).json({ token });
+  } catch (error) {
+    next(error);
+  }
+});
 
 // TODO: POST /login
+router.post("/login", async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const customer = await prisma.customer.login(email, password);
+    const token = createToken(customer.id);
+    res.json({ token });
+  } catch (error) {
+    next(error);
+  }
+});
 
 /** Checks the request for an authenticated customer. */
 function authenticate(req, res, next) {
--- prisma/index.js
+++ prisma/index.js
@@ -4,7 +4,24 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient().$extends({
   model: {
     customer: {
-      // TODO: Add register and login methods
+      async register(email, password) {
+        const hashPassword = await bcrypt.hash(password, 10);
+        const newCustomer = await prisma.customer.create({
+          data: {
+            email,
+            password: hashPassword,
+          },
+        });
+        return newCustomer;
+      },
+      async login(email, password) {
+        const customer = await prisma.customer.findUniqueOrThrow({
+          where: { email },
+        });
+        const valid = await bcrypt.compare(password, hashPassword);
+        if (!valid) throw Error("Invalid password");
+        return customer;
+      },
     },
   },
 });
